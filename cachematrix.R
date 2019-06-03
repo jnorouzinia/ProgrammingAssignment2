4e681c1c6abe570666ea5bdeae2dd7e4b87ca7f3
Javad Norouzi Nia <jnorouzinia@wpi.edu>
2019-06-03 19:10:16 -0400

    My answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix and cacheSolve functions reports the inverse of input matrix.
+## If the inverted matrix is available since previous computations, it 
+## does not repeat it and uses the cashed result.
 
-## Write a short comment describing this function
+
+## MakeCacheMatrix recieves a matrix and make closure for the function. If it 
+## is the first time that you run the function, the output of "get" function 
+## would be the input and output of "getinv" would be NULL. 
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Inv <- NULL
+        set <- function(y) {
+                x <<- y
+                Inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(Inverse) Inv <<- Inverse
+        getInv <- function() Inv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## By running cacheSolve function,
+## it tries to check if the inverse of matrix is available
+## or not. If is not, it calculate it and will show the result, and if is, it
+## prints "getting cached data" and then the inverse with repeating the 
+## computation.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        Inv <- x$getInv()
+        if(!is.null(Inv)) {
+                message("getting cached data")
+                return(Inv)
+        }
+        Inv <- solve(x$get())
+        x$setInv(Inv)
+        Inv
 }
